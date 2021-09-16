d4b7ed76a409c5e023a5f1a4785c78ec9b112163
sws1552 <sws1552@gmail.com>
2021-09-16 14:04:23 +0900

    공통 모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,27 +1,3 @@
-const searchEl = document.querySelector('.search');
-// console.log(searchEl);
-const searchInputEl = searchEl.querySelector('input');
-// console.log(searchInputEl);
-
-searchEl.addEventListener('click', function () {
-  searchInputEl.focus();
-});
-
-searchInputEl.addEventListener('focus', function () {
-  searchEl.classList.add('focused');
-  searchInputEl.setAttribute('placeholder', '통합검색');
-});
-
-searchInputEl.addEventListener('blur', function () {
-  searchEl.classList.remove('focused');
-  searchInputEl.setAttribute('placeholder', '');
-});
-
-
-
-
-
-
 
 const badgeEl = document.querySelector('header .badges');
 
@@ -185,5 +161,3 @@ spyEls.forEach(function (spyEl) {
 
 
 
-const thisYear = document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear();
